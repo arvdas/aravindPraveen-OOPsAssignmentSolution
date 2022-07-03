df5ae1c57810ed4d485cee7bba56aaebd0abe9ea
arvdas <105780060+arvdas@users.noreply.github.com>
2022-07-03 16:46:55 +0530

    Add files via upload

---
+++ aravindgradedassignment1/src/com/greatlearning/main/adminDepartment.java
@@ -0,0 +1,23 @@
+package com.greatlearning.main;
+
+import com.greatlearning.departments.superDepartment;
+
+class adminDepartment extends superDepartment 
+ {
+	 public String departmentName()
+	{
+		return "Admin Department";					
+	}
+	
+	public String getTodaysWork()
+	{
+		return "Complete your documents Submission";	
+	}
+	public String getWorkDeadline()
+	{
+		return "Complete by EOD";						
+	}
+
+	 
+
+ }
